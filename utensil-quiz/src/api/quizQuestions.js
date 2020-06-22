759c3a5c7e8902c7a04fbe5ae51213128e2f453a
Brian Francis De Guzman <deguzmanbrianfrancis@gmail.com>
2020-06-21 21:55:11 -0400

    new questions added

--- utensil-quiz/src/api/quizQuestions.js
+++ utensil-quiz/src/api/quizQuestions.js
@@ -1,89 +1,194 @@
 var quizQuestions = [
     {
-        question: "What franchise would you rather play a game from?",
+        question: "The last time a coworker/family member came to you about a problem with another person, you...",
         answers: [
             {
-                type: "Microsoft",
-                content: "Halo"
+                type: "Whisk",
+                content: "Acted as a mediator between the two, helping them reach a compromise"
             },
             {
-                type: "Nintendo",
-                content: "Pokemon"
+                type: "Wooden Spoon",
+                content: "Were a good listener, and offered a few possible approaches (if asked)"
             },
             {
-                type: "Sony",
-                content: "Uncharted"
+                type: "Spatula",
+                content: "Came up with a slightly crazy solution...that worked!"
+            },
+            {
+                type: "Chef’s Knife",
+                content: "Helped them process the problem into solvable steps."
             }
         ]
     },
     {
-        question: "Which console would you prefer to play with friends?",
+        question: "When presented with this list of adjectives, people are most likely to describe you as...",
         answers: [
             {
-                type: "Microsoft",
-                content: "X-Box"
+                type: "Whisk",
+                content: "Charismatic"
             },
             {
-                type: "Nintendo",
-                content: "Nintendo 64"
+                type: "Wooden Spoon",
+                content: "Reliable"
             },
             {
-                type: "Sony",
-                content: "Playstation 1"
+                type: "Spatula",
+                content: "Creative"
+            },
+            {
+                type: "Chef’s Knife",
+                content: "Decisive"
             }
         ]
     },
     {
-        question: "Which of these racing franchises would you prefer to play a game from?",
+        question: "When presented with this list of adjectives, people are most likely to describe you as...",
         answers: [
             {
-                type: "Microsoft",
-                content: "Forza"
+                type: "Whisk",
+                content: "Fake"
             },
             {
-                type: "Nintendo",
-                content: "Mario Kart"
+                type: "Wooden Spoon",
+                content: "Sloppy"
             },
             {
-                type: "Sony",
-                content: "Gran Turismo"
+                type: "Spatula",
+                content: "Rash"
+            },
+            {
+                type: "Chef’s Knife",
+                content: "Controlling"
             }
         ]
     },
     {
-        question: "Which of these games do you think is best?",
+        question: "In your friend group you are the...",
         answers: [
             {
-                type: "Microsoft",
-                content: "BioShock"
+                type: "Whisk",
+                content: "One who keeps everyone together"
             },
             {
-                type: "Nintendo",
-                content: "The Legend of Zelda: Ocarina of Time"
+                type: "Wooden Spoon",
+                content: "One who is always available"
             },
             {
-                type: "Sony",
-                content: "Final Fantasy VII"
+                type: "Spatula",
+                content: "One who keeps everyone laughing"
+            },
+            {
+                type: "Chef’s Knife",
+                content: "One who makes the plans"
             }
         ]
     },
     {
-        question: "What console would you prefer to own?",
+        question: "A group project deadline is fast approaching and you might not finish in time. You...",
         answers: [
             {
-                type: "Microsoft",
-                content: "X-Box One"
+                type: "Whisk",
+                content: "Motivate everyone and yourself with well-timed jokes/compliments, and ask for a little more time if you can."
             },
             {
-                type: "Nintendo",
-                content: "Wii U"
+                type: "Wooden Spoon",
+                content: "Help multiple people with small parts of their assigned roles once you finish yours."
             },
             {
-                type: "Sony",
-                content: "Playstation 4"
+                type: "Spatula",
+                content: "Come up with a new, quicker way of finishing the project based on a loophole."
+            },
+            {
+                type: "Chef’s Knife",
+                content: "Break up the parts of the project that might be late, and delegating pieces out to people who have the time/ability."
             }
         ]
-    }
+    },
+    {
+        question: "You and a friend/partner fight about a strong difference of opinion. How do you rectify the situation?",
+        answers: [
+            {
+                type: "Whisk",
+                content: "Apologize, accept their apology, and work together towards a meaningful compromise. If compromise cannot be found, end it amicably."
+            },
+            {
+                type: "Wooden Spoon",
+                content: "The next time you see them, it’s like nothing ever happened. The fight fades into the past, with the chance of it resurfacing at a later time."
+            },
+            {
+                type: "Spatula",
+                content: "Cool off, then approach the argument again with a new line of thinking or debate strategy. If you don’t reach a conclusion, rinse and repeat."
+            },
+            {
+                type: "Chef’s Knife",
+                content: "Lay out a plan for how you both can get along in spite of the differences. If they aren’t receptive and the fight was bad enough, you might even cut off the relationship."
+            }
+        ]
+    },
+    {
+        question: "A neighbor keeps leaving their dog poop on your sidewalk/lawn/etc. Provided you know who it is, you...",
+        answers: [
+            {
+                type: "Whisk",
+                content: "Befriend them, then get them to stop."
+            },
+            {
+                type: "Wooden Spoon",
+                content: "Ask them to stop. If that doesn’t work, pick it up yourself or just ignore it."
+            },
+            {
+                type: "Spatula",
+                content: "Pick up the poop. Deposit it their mailbox."
+            },
+            {
+                type: "Chef’s Knife",
+                content: "Contact the landlord/HOA/super/city hall/etc."
+            }
+        ]
+    },
+    {
+        question: "Since no personality quiz is complete without a question like this, pick a pet:",
+        answers: [
+            {
+                type: "Whisk",
+                content: "Dog"
+            },
+            {
+                type: "Wooden Spoon",
+                content: "Guinea Pig"
+            },
+            {
+                type: "Spatula",
+                content: "Rat"
+            },
+            {
+                type: "Chef’s Knife",
+                content: "Cat"
+            }
+        ]
+    },
+    {
+        question: "...And a color.",
+        answers: [
+            {
+                type: "Whisk",
+                content: "Yellow"
+            },
+            {
+                type: "Wooden Spoon",
+                content: "White"
+            },
+            {
+                type: "Spatula",
+                content: "Red"
+            },
+            {
+                type: "Chef’s Knife",
+                content: "Black"
+            }
+        ]
+    },
+
   ];
   
   export default quizQuestions;
