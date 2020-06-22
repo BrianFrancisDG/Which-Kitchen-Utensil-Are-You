38d2a68942fa22ab1f289f53e4c55fbf2188a74a
Brian Francis De Guzman <deguzmanbrianfrancis@gmail.com>
2020-06-21 22:06:14 -0400

    Slight styling update

--- utensil-quiz/src/App.js
+++ utensil-quiz/src/App.js
@@ -125,8 +125,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Quiz</h2>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h2>Which Kitchen Utensil Are You?</h2>
         </div>
         {this.state.result ? this.renderResult() : this.renderQuiz()}
       </div>
